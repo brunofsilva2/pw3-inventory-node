d667899a26c4edc83056a17cd953a4fac8bd9789
brunofsilva2 <bruno.19fs@gmail.com>
2024-10-17 13:25:17 -0300

    fix: corrigindo erros do backend e validando a api de cadastro de usuários

--- src/services/userService.js
+++ src/services/userService.js
@@ -9,21 +9,13 @@ async function createUser(name, email, password, user_type) {
     const conn = await database.connect();
 
     try {
-        const [existingEmail] = await conn.query('select 1 from usuarios where email = ?', [email]);
+        const userData = "insert into users(user_name, email, password_hash, user_type) values (?, ?, ?, ?)";
 
-        if (existingEmail.length > 0) {
-            return { success: false, message: "Já existe um usuário cadastrado com esse e-mail." };
-        }
-        
-            const userData = "insert into usuarios(nome_usuario, email, senha, permissao) values (?, ?, ?, ?)";
+        const dataUser = [name, email, password, user_type];
+        await conn.query(userData, dataUser);
 
-            const dataUser = [name, email, password, user_type];
-            await conn.query(userData, dataUser);
-            console.log('oiii', dataUser)
-
-            return { success: true, message: "Usuário criado com sucesso."};
+        return { success: true, message: "Usuário criado com sucesso."};
     } catch (error) {
-        console.error('Erro ao criar usuário:', error.message); // Log do erro
         return { success: false, message: "Erro ao criar usuário." + error.message };
     } finally {
         await conn.end();
