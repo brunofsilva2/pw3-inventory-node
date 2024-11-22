4dbf19b69b939aa581f5d9cdca09082f81d20132
brunofsilva2 <bruno.19fs@gmail.com>
2024-11-21 21:22:10 -0300

    feat: adicionando produtos e criando botão de e

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,16 +1,42 @@
 import express from 'express';
-import service from '../services/productsService.js';
+import productsService from '../services/productsService.js';
 
 const routes = express.Router();
 
 routes.get('/', async (request, response) => {
-    const products = await service.listProducts();
+    try {
+        const products = await productsService.listProducts();
 
-    if(products.length < 1) {
-        return response.status(204).end();
+        if (products.length < 1) {
+            return response.status(404).send({ message: "Nenhum produto encontrado." });
+        }
+
+        response.status(200).send(products);
+    } catch (error) {
+        console.error(error);
+        response.status(500).send({ message: "Erro ao recuperar produtos." });
     }
-
-    response.status(200).send({message: products});
 });
+
+
+routes.delete('/:id', async (req, res) => {
+    const productId = parseInt(req.params.id, 10);
+
+    try {
+
+        const result = await productsService.deleteProducts(productId);
+
+        if (!result.success) {
+            return res.status(404).send({ success: false, message: "Produto não encontrado." });
+        }
+
+        res.status(200).send({ success: true, message: result.message });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, message: 'Erro interno ao remover o produto.' });
+    }
+});
+
 
 export default routes;
--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,5 +1,19 @@
 import database from '../repository/mysql.js'
 
+async function deleteProducts(id){
+    const conn = await database.connect();
+
+    try {
+        const sql = "DELETE FROM Products WHERE id = ?"
+        await conn.query(sql, [id]);
+        return { success: true, message: `Produto deletado com sucesso.`}
+    } catch (error) {
+        return { success: false, message: error.message}
+    } finally {
+        conn.end();
+    }
+}
+
 async function listProducts(){
     const conn = await database.connect();
     const sql = "SELECT * FROM products";
@@ -8,4 +22,4 @@ async function listProducts(){
     return rows;
 }
 
-export default {listProducts}
+export default {listProducts, deleteProducts}
